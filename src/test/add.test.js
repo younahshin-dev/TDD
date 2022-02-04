55ed7b0dd98e0c291f79dd312441c01b1cf0eb80
younahshin-dev <roaving@gmail.com>
2022-02-04 13:10:46 +0900

    create add.test.js, npm install @types/jest

---
+++ src/test/add.test.js
@@ -0,0 +1,8 @@
+const { TestWatcher } = require('jest');
+const add = require('../add.js');
+
+//테스트 함수를 ''원하는 타이틀로 실행하고 콜백함수 안에서 테스트 진행
+test('', () => {
+  //테스트 코드 작성
+  expect(add(1, 2)).toBe(3);
+})
