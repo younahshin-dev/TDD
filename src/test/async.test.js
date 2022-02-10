b0c7b095bda4ba7d9db34999eb0d99b503c9afaa
younahshin-dev <roaving@gmail.com>
2022-02-10 13:21:00 +0900

    add more async test

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -1,6 +1,25 @@
 const fetchProduct = require('../async.js');
 
 describe('async test', () => {
+  it('asyc-done way', (done) => {
+    fetchProduct().then((item) => {
+      expect(item).toEqual({ item: 'Milk', price: 200});
+      done(); // 5ms 정도 시간 소요
+    });
+  });
+
+  it('asyc-return way', () => {
+    return fetchProduct().then((item) => {
+      expect(item).toEqual({ item: 'Milk', price: 200}); //1ms 바로 리턴
+    });
+  });
+
+  it('asyc-await way',async () => {
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: 'Milk', price: 200}); 
+    });
+  });
+
   it('promise Reject', () => {
     return expect(fetchProduct('error')).rejects.toMatch('network error');
   });
